e10c4d8391a8b2cb5646ea921c306ba8da7e1486
jhonzoni19-creator <jhonzoni19@gmail.com>
2026-08-03 10:00:29 -0300

    Create 3472040.lua

---
+++ config/stplug-in/3472040.lua
@@ -0,0 +1,19 @@
+addappid(3472040) -- NBA 2K26
+addappid(3472041, 1, "98720ddedf5fb5a37faf70ca90602a9949e727a7f7b6b9171ecc2764ee2d3b21") -- Depot 3472041
+setManifestid(3472041, "3745295277215675478", 76080228004)
+addappid(3825290)
+addappid(3472042, 1, "34db1ecbd759eab0b3e63e7304bc8fe901b1ca351a3d453affb865d97c5e62be") -- High Resolution Textures - Depot 3472042
+setManifestid(3472042, "3352108571045691532", 28033916597)
+addappid(3657560) -- NBA 2K26 SLAM Edition Bonus Pack Offer
+addappid(3657590) -- NBA 2K26 Pro Pass Season 1
+addappid(3657600) -- NBA 2K26 Hall of Fame Pass Season 1
+addappid(3657610) -- NBA 2K26 MyCAREER Bonus Offer Season 1
+addappid(3657620) -- NBA 2K26 MyTEAM Bonus Offer Season 1
+addappid(3657640) -- NBA 2K26 Pro Pass Season 2
+addappid(3657650) -- NBA 2K26 Hall of Fame Pass Season 2
+addappid(3657660) -- NBA 2K26 MyCAREER Bonus Offer Season 2
+addappid(3657670) -- NBA 2K26 MyTEAM Bonus Offer Season 2
+addappid(3657680) -- NBA 2K26 Pro Pass Season 3
+addappid(3657690) -- NBA 2K26 Hall of Fame Pass Season 3
+addappid(3657700) -- NBA 2K26 MyCAREER Bonus Offer Season 3
+addappid(3657710) -- NBA 2K26 MyTEAM Bonus Offer Season 3
